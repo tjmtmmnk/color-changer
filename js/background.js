9ec43fb666a967f75fb1e1cfd58232500178f3c8
tjmtmmnk <tsujimoto027@gmail.com>
2019-10-22 16:14:23 +0900

    コマンドで受けれなかったのでアイコンをクリックした時に動作させる

--- js/background.js
+++ js/background.js
@@ -1,48 +1,23 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.browserAction.getBadgeText({}, function(text) {
-
-    if (text==="on") {
-      chrome.browserAction.setBadgeText({ text: "" });
-      return;
-    }
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.browserAction.getBadgeText({}, function (text) {
 
     chrome.browserAction.setBadgeText({ text: "on" });
-    chrome.browserAction.setBadgeBackgroundColor({ color: "black"});
+    chrome.browserAction.setBadgeBackgroundColor({ color: "black" });
 
-  });
-});
-
-chrome.commands.onCommand.addListener(function (command) {
-  chrome.browserAction.getBadgeText({}, function(text) {
-
-    if (text==="") {
-      return;
-    }
-
-    // カラー設定
     var msg = {};
-    switch (command) {
-      case "red":
-        msg.color = "red";
-        break;
-      case "blue":
-        msg.color = "blue";
-        break;
-    }
+    msg.color = "blue";
 
-    // 取得するタブの条件
     var queryInfo = {
       active: true,
-      windowId: chrome.windows.WINDOW_ID_CURRENT
+      currentWindow: true,
     };
 
-    // タブの情報を取得する
     chrome.tabs.query(queryInfo, function (result) {
       var currentTab = result.shift();
+      console.log(currentTab);
 
       // 現在表示しているタブにメッセージを送る
-      chrome.tabs.sendMessage(currentTab.id, msg, function() {});
+      chrome.tabs.sendMessage(currentTab.id, msg, function () { });
     });
-
   });
 });
