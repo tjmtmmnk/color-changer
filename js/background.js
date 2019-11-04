f62bbd7ce3e1110e8563cabdc8411b49d4111025
tjmtmmnk <tsujimoto027@gmail.com>
2019-11-04 14:26:12 +0900

    コマンドで文字を青くする

--- js/background.js
+++ js/background.js
@@ -1,23 +1,17 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.browserAction.getBadgeText({}, function (text) {
+  var msg = {};
+  msg.color = "blue";
 
-    chrome.browserAction.setBadgeText({ text: "on" });
-    chrome.browserAction.setBadgeBackgroundColor({ color: "black" });
+  var queryInfo = {
+    active: true,
+    currentWindow: true,
+  };
 
-    var msg = {};
-    msg.color = "blue";
+  chrome.tabs.query(queryInfo, function (result) {
+    var currentTab = result.shift();
+    console.log(currentTab);
 
-    var queryInfo = {
-      active: true,
-      currentWindow: true,
-    };
-
-    chrome.tabs.query(queryInfo, function (result) {
-      var currentTab = result.shift();
-      console.log(currentTab);
-
-      // 現在表示しているタブにメッセージを送る
-      chrome.tabs.sendMessage(currentTab.id, msg, function () { });
-    });
+    // 現在表示しているタブにメッセージを送る
+    chrome.tabs.sendMessage(currentTab.id, msg, function () { });
   });
 });
